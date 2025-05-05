c81d63f25e552a23eb9ace48b6649c60c3bfa393
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-05-05 17:35:08 +0300

    Fixed build.py

--- src/build.py
+++ src/build.py
@@ -2,10 +2,10 @@ import backend.users_commands
 import backend.activities_commands
 
 def initialize_database():
-    users_commands.drop_table_users()
-    activities_commands.drop_table_activities()
-    users_commands.create_table_users()
-    activities_commands.create_table_activities()
+    backend.users_commands.drop_table_users()
+    backend.activities_commands.drop_table_activities()
+    backend.users_commands.create_table_users()
+    backend.activities_commands.create_table_activities()
 
 if __name__ == "__main__":
     initialize_database()
